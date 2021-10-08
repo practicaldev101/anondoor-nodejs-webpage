fe04ff98508b43c8d2862b819ea33980fffd272c
practicaldev101 <carlosospinomonsalve@gmail.com>
2021-10-08 15:42:35 +0000

    Web Page added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const app = express()
 const puerto = 3000;
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname +  "/index.html")
+    res.sendFile(__dirname +  "/views/home.html");
+    app.use(express.static("theme"));
 })
 
 app.listen(puerto, ()=>{
